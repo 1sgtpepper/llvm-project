0e48409311a1a3fcff7714c5e3adf4aab048771c
1sgtpepper <cynejarviszarceno@gmail.com>
2026-09-13 17:10:29 +0800

    [SelectionDAG] Cache successor distances while nodes are queued

--- llvm/lib/CodeGen/SelectionDAG/ScheduleDAGRRList.cpp
+++ llvm/lib/CodeGen/SelectionDAG/ScheduleDAGRRList.cpp
@@ -1724,6 +1724,10 @@ protected:
   // SethiUllmanNumbers - The SethiUllman number for each node.
   std::vector<unsigned> SethiUllmanNumbers;
 
+  // A queued node's successors are already scheduled. Their heights remain
+  // fixed until the node leaves the queue or its dependencies are updated.
+  DenseMap<const SUnit *, unsigned> ClosestSuccs;
+
   /// RegPressure - Tracking current reg pressure per register class.
   std::vector<unsigned> RegPressure;
 
@@ -1769,10 +1773,13 @@ public:
   void releaseState() override {
     SUnits = nullptr;
     SethiUllmanNumbers.clear();
+    ClosestSuccs.clear();
     llvm::fill(RegPressure, 0);
   }
 
   unsigned getNodePriority(const SUnit *SU) const;
+
+  unsigned getClosestSucc(const SUnit *SU);
 
   unsigned getNodeOrdering(const SUnit *SU) const {
     if (!SU->getNode()) return 0;
@@ -1795,6 +1802,7 @@ public:
     if (I != std::prev(Queue.end()))
       std::swap(*I, Queue.back());
     Queue.pop_back();
+    ClosestSuccs.erase(SU);
     SU->NodeQueueId = 0;
   }
 
@@ -1879,6 +1887,7 @@ public:
     if (Queue.empty()) return nullptr;
 
     SUnit *V = popFromQueue(Queue, Picker, scheduleDAG);
+    ClosestSuccs.erase(V);
     V->NodeQueueId = 0;
     return V;
   }
@@ -2006,6 +2015,7 @@ void RegReductionPQBase::addNode(const SUnit *SU) {
 }
 
 void RegReductionPQBase::updateNode(const SUnit *SU) {
+  ClosestSuccs.erase(SU);
   SethiUllmanNumbers[SU->NodeNum] = 0;
   CalcNodeSethiUllmanNumber(SU, SethiUllmanNumbers);
 }
@@ -2334,6 +2344,13 @@ static unsigned closestSucc(const SUnit *SU) {
   return MaxHeight;
 }
 
+unsigned RegReductionPQBase::getClosestSucc(const SUnit *SU) {
+  auto [It, Inserted] = ClosestSuccs.try_emplace(SU, 0);
+  if (Inserted)
+    It->second = closestSucc(SU);
+  return It->second;
+}
+
 /// calcMaxScratches - Returns an cost estimate of the worse case requirement
 /// for scratch registers, i.e. number of data dependencies.
 static unsigned calcMaxScratches(const SUnit *SU) {
@@ -2580,8 +2597,8 @@ static bool BURRSort(SUnit *left, SUnit *right, RegReductionPQBase *SPQ) {
   // t3 = op t4, c2
   //
   // This creates more short live intervals.
-  unsigned LDist = closestSucc(left);
-  unsigned RDist = closestSucc(right);
+  unsigned LDist = SPQ->getClosestSucc(left);
+  unsigned RDist = SPQ->getClosestSucc(right);
   if (LDist != RDist)
     return LDist < RDist;
 
